0de616ac00bb40ef991d323e100f4e2896994903
Янин Ярослав <yaroslav.i.yanin@gmail.com>
2026-07-15 17:49:12 +0500

    реализовал перестановку элементов списка

--- lib/src/list.c
+++ lib/src/list.c
@@ -32,6 +32,48 @@ ListEntry* list_remove(ListEntry* entry) {
     return entry;
 }
 
+// Функция меняет местами два элемента в списке
+// <entry1> - первый переставляемый элемент
+// <entry2> - второй переставляемый элемент
+void list_swap(ListEntry* entry1, ListEntry* entry2) {
+    ListEntry* entry1_next, * entry1_prev;
+    if (entry1 == entry2) return; // сам с собою элемент не переставляем
+
+    if (entry1->next == entry2) {
+        // перстановка соседних элементов, entry1 слева от entry2
+        // Аккуратно перевязываем 6 указателей (2 у соседей снаружи, 4 у самих элементов)
+        entry1->prev->next = entry2;
+        entry2->next->prev = entry1;
+        entry2->prev = entry1->prev;
+        entry1->next = entry2->next;
+        entry1->prev = entry2;
+        entry2->next = entry1;
+
+    }
+    else if (entry2->next == entry1) {
+        // перстановка соседних элементов, entry1 справа от entry2
+        list_swap(entry2, entry1);  // Вызываем эту же функцию, поменяв аргументы местами
+    }
+    else {
+        // элементы не граничат друг с другом
+        // Сохраняем старых соседей первого элемента
+        entry1_next = entry1->next;
+        entry1_prev = entry1->prev;
+
+        // Перевязываем соседей так, чтобы они указывали на новые места
+        entry1_prev->next = entry2;
+        entry1_next->prev = entry2;
+        entry2->prev->next = entry1;
+        entry2->next->prev = entry1;
+
+        // Меняем указатели самих элементов
+        entry1->next = entry2->next;
+        entry1->prev = entry2->prev;
+        entry2->next = entry1_next;
+        entry2->prev = entry1_prev;
+    }
+}
+
 // Функция возвращает указатель на следующий элемент списка
 // <entry> - текущий элемент
 ListEntry* list_next(ListEntry* entry) {
